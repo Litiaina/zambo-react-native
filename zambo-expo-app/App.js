53cfd2622f31adf198707247f68b0c9e9aa997af
ZAMBO-SYSARCH-3A <demowillzam123@gmail.com>
2023-05-18 08:01:42 +0800

    Machine Problem #2 & 3

--- zambo-expo-app/App.js
+++ zambo-expo-app/App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
+  const handleButtonClick = () => {
+    Alert.alert('Hello, World!');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Click Me" onPress={handleButtonClick} />
     </View>
   );
 }
